ed9640a960756f78e6335509bb35b3f8c4572c9a
Andrew Nagle <andrew@Andrews-MacBook-Pro.local>
2017-10-23 11:26:09 -0500

    updated styling and added user session functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const parseUrl = require('parseurl');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
@@ -9,7 +10,8 @@ require('./db/db');
 
 const tvController = require('./controllers/tv');
 const homeController = require('./controllers/home');
-const moviesController = require('./controllers/movies')
+const moviesController = require('./controllers/movies');
+const session = require('express-session')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -17,6 +19,36 @@ app.set('views', __dirname + '/views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+
+app.use(session({
+	secret: 'super duper secret string that you cant know so dont ask',
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(function(req, res, next){
+	if(parseUrl(req).pathname.includes('movie')){
+		if(req.session.logged === true){
+			next();
+		} else {
+		res.redirect('/account');
+		}
+	} else{
+		next();
+	}	
+});
+
+app.use(function(req, res, next){
+	if(parseUrl(req).pathname.includes('tv')){
+		if(req.session.logged === true){
+			next();
+		} else {
+		res.redirect('/account');
+		}
+	} else{
+		next();
+	}	
+});
 
 app.use( function( req, res, next ) {
     if ( req.query._method === 'DELETE' ) {
